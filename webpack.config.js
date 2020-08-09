504a16e00db6eab579cdf20e53004e6c43abc00d
Christian Miller <cnmiller127@gmail.com>
2020-08-09 12:04:44 -0500

    initial setup

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+// Temlate
+
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
+const config = {
+  entry: "./src/app.js",
+  output: {
+    path: __dirname + "/dist",
+    filename: "bundle.js"
+  },
+  mode: "development",
+  plugins: [new BundleAnalyzerPlugin()]
+};
+module.exports = config;
